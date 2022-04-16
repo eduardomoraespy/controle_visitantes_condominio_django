bbceb768a64516751fa2991d97058996cadb0228
eduardomoraespy <eduardomoraes2213@hotmail.com>
2022-04-16 01:09:26 -0300

    templates ajustados

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from usuarios import views
+from usuarios.views import home
+from visitantes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.home, name='home')
+    path('', home, name='home'),
+
+    path('visitante/cadastro', views.registrar_visitante, name='registrar_visitante')
 ]
--- visitantes/views.py
+++ visitantes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def registrar_visitante(request):
+
+    context = {
+
+    }
+
+    return render(request, 'registrar_visitante/registrar_visitante.html', context)
